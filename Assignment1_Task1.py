2ceced65357d68708edc7d10eba23c479e03e79b
aishik92-lgtm <aishik92@gmail.com>
2026-02-19 23:59:30 +0530

    initial commit

---
+++ Assignment1_Task1.py
@@ -0,0 +1,15 @@
+"""
+Calculator operations
+"""
+
+a1 = int(input("Enter the first number: "))
+a2 = int(input("Enter the second number: "))
+
+addition  = a1 + a2
+subtraction = a1 - a2
+multiplication = a1 * a2
+division = a1 / a2
+print("Addition: ",addition)
+print("Subtraction: ",subtraction)
+print("Multiplication: ",multiplication)
+print("Division: ",division)
