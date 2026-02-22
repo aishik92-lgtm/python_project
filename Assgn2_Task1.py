2d60ade59a5a6b1af7bf488f94878232805ae932
aishik92-lgtm <aishik92@gmail.com>
2026-02-22 15:01:34 +0530

    Initial commit

---
+++ Assgn2_Task1.py
@@ -0,0 +1,12 @@
+# Check a user input number is Even or Odd
+
+#taking input
+num = int(input("Enter a number: "))
+
+# Conditional block to find the number for Even or odd
+
+if num % 2 == 0:
+    print(f"{num} is an even number")
+else:
+    print(f"{num} is an odd number")
+
