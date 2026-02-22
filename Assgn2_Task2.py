3ca1a7fd49c82528d60170178a2cc44b83effe97
aishik92-lgtm <aishik92@gmail.com>
2026-02-22 15:02:01 +0530

    Initial commit

---
+++ Assgn2_Task2.py
@@ -0,0 +1,12 @@
+# Sum of all the integers from 1 to 50
+
+#initialize a variable
+sum = 0
+
+#for loop for identifying range
+for i in range(1,51):
+    sum = sum + i
+
+#printing the sum value
+print(f"The sum of numbers from 1 to 50 is: {sum}")
+
